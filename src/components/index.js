8cf18770383ee5c25c5f3d5a9e25836da403052b
Manohar <manoharkethavath1230@gmail.com>
2023-09-21 20:07:48 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Billing from './Billing';
+import  Stats  from './Stats';
+ import Business from './Business'
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+
+import Footer from './Footer';
+
+import Testimonials from './Testimonials';
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+
+
+
+};
+
